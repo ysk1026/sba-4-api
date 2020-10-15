c6fd97cbf76aeb7e9e7120e7479ed86c3583d880
Kim <fkqoseka@gmail.com>
2020-10-15 12:24:22 +0900

    14:24

--- com_sba_api/user/user.py
+++ com_sba_api/user/user.py
@@ -1,8 +1,18 @@
 from com_sba_api.ext.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.mysql import DECIMAL, VARCHAR, LONGTEXT
+ 
 class User(Base):
     __tablename__ = "Users"
     
     id = Column(Integer, primary_key=True, index=True)
+    userid = Column(String)
+    userpw = Column(String)
     
+engine = create_engine('mysql+mysqlconnector://root:root@127.0.0.1/mariadb?charset=utf8', encoding='utf8', echo=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind = engine)
+session = Session()
+
+session.commit()
